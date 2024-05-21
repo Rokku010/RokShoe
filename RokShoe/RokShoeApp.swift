5429b0616d1ba7b490e107b73c9557b063ae3298
Rokku <quocloctran357@gmail.com>
2024-05-22 05:44:38 +0700

    Initial Commit

---
+++ RokShoe/RokShoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RokShoeApp.swift
+//  RokShoe
+//
+//  Created by Quốc Lộc on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RokShoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
